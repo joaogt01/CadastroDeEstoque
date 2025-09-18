0e0a7d081224227340cc1df008aa6c79f4b7cc56
João Victor <joao.dev06@gmail.com>
2025-09-17 21:30:13 -0300

    Initial commit

---
+++ src/main/java/com/estoque/CadastroDeEstoque/CadastroDeEstoqueApplication.java
@@ -0,0 +1,13 @@
+package com.estoque.CadastroDeEstoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeEstoqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeEstoqueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estoque/CadastroDeEstoque/CadastroDeEstoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estoque.CadastroDeEstoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeEstoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
