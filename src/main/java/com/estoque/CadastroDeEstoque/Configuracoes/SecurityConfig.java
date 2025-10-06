4af5766d363f2ff5bb8f3c4fc3aeff0de2aa8f7e
João Victor <joao.dev06@gmail.com>
2025-10-06 18:15:17 -0300

    fix: Corrige as rotas para implementar o Security

--- src/main/java/com/estoque/CadastroDeEstoque/Configuracoes/SecurityConfig.java
+++ src/main/java/com/estoque/CadastroDeEstoque/Configuracoes/SecurityConfig.java
@@ -14,12 +14,13 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
-    //private final SecurityFilter securityFilter;
+    private final SecurityFilter securityFilter;
 
 
 
@@ -35,7 +36,7 @@ public class SecurityConfig {
                         .anyRequest().authenticated()
                 )
                 .headers(headers -> headers.frameOptions(frame -> frame.disable()))
-                //.addFilter(new JwtAuthenticationFilter(...)) // 
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
